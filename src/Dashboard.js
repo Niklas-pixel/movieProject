3e0f0148a830780a40f7c6a5a1f97dc0efa12242
Niklas Wernfeldt <niklas.wernfeldt.94@gmail.com>
2020-11-01 17:26:00 +0100

    dashboard.html - add rough css

---
+++ src/Dashboard.js
@@ -0,0 +1,32 @@
+const watchlist = document.querySelector(".watchlist");
+const viewedMovies = document.querySelector(".viewed-movies");
+
+const imgBase200 = "https://image.tmdb.org/t/p/w200";
+
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=a2bf12ab60f87e1ff69ef7b00f747938"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // console.log(data.results[0])
+
+    data.results.forEach((topMovie) => {
+      // Below is for dashboard watchlist
+      const srcUrl200 = imgBase200 + topMovie.poster_path;
+      const imgTagsTopRated = document.createElement("img");
+      imgTagsTopRated.setAttribute("src", srcUrl200);
+      imgTagsTopRated.setAttribute("class", ".card");
+      watchlist.appendChild(imgTagsTopRated);
+
+      // Below is for dashboard viewed movies
+      const imgTagsViewed = document.createElement("img");
+      imgTagsViewed.setAttribute("src", srcUrl200);
+      imgTagsViewed.setAttribute("class", ".card");
+      viewedMovies.appendChild(imgTagsViewed);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
