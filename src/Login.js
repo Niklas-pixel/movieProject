81704e7ad0d1f1f9654fdee2d0c968a4be87ee5f
Niklas Wernfeldt <niklas.wernfeldt.94@gmail.com>
2020-11-03 10:46:44 +0100

    add Login class

---
+++ src/Login.js
@@ -0,0 +1,36 @@
+class Login {
+  constructor() {
+    this.emailInput = document.querySelector("#email-input");
+    this.passwordInput = document.querySelector("#password-input");
+    this.loginButton = document.querySelector("#login-btn");
+    this.loginError = document.querySelector(".login-error");
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    const users = db.getAllUsers();
+
+    const email = this.emailInput.value;
+    const password = this.passwordInput.value;
+
+    const user = users.find((user) => {
+      if (user.email === email && user.password === password) {
+        return true;
+      }
+    });
+
+    if (!user) {
+      this.loginError.innerHTML = "password or email is wrong";
+    } else if (user) {
+      location.assign("dashboard.html");
+    }
+  };
+
+  addListeners = () => {
+    this.loginButton.addEventListener("click", this.handleLogin);
+  };
+}
+
+const login = new Login();
+
+window.addEventListener("load", login.addListeners);
